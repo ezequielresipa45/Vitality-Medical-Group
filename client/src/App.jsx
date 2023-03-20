f7f8ac5a61eb41f9ad808e00bd63a999c1e64379
Ezequiel <ezequielresipa45@gmail.com>
2023-03-20 14:31:35 -0300

    Add folders Client and Api

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import './App.css'
+import NavBar from "./components/NavBar/NavBar";
+
+function App() {
+
+  return (
+    <div className="App">
+
+     <h2>Soy el Home</h2>
+
+     <Routes>
+        <Route path="/prueba" element={<NavBar />} />
+      </Routes>
+
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/redux/reducer.js
@@ -0,0 +1,19 @@
+
+const initialState = {
+    testState: [],
+  };
+  
+ 
+  const reducer = (state = initialState, action) => {
+    switch (action.type) {
+
+        // cases ....
+  
+      default:
+        return {
+          ...state,
+        };
+    }
+  };
+  
+  export default reducer;
