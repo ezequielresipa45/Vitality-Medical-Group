ab6d9458957bab4471e6825b819d8efd249c92ec
Ezequiel <ezequielresipa45@gmail.com>
2023-03-21 14:24:47 -0300

    Working in NavBar Styles

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import styles from "./NavBar.module.css";
+import logo from "../../images/logo.jpeg"
 
 export default function NavBar() {
   return (
-    <div>NavBar</div>
+    <div className={styles.container__navbar}>
+
+      <div className={styles.container__logo}>
+      <img src={logo} alt="logo" />
+      <p>Vitality Medical Group</p>
+      </div>
+
+<ul>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+<li></li>
+</ul>
+
+
+
+    </div>
   )
 }
