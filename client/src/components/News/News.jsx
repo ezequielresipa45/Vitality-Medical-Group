f2cece17573ad33e81958082c9da409f95d94be2
Emanuel <marquezema127@gmail.com>
2023-03-23 00:07:54 -0300

    main fixed & news added

---
+++ client/src/components/News/News.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from './News.module.css';
+
+
+export default function News(props) {
+
+    return (
+
+        <div className={styles.news_container}>
+
+            <img src={props.image} alt='news image'/>
+
+            <div className={styles.item}>
+
+                <h2>{props.title}</h2>
+
+                <p>{props.description}</p>
+
+                <button>
+                    <Link to={props.url}>Read more</Link>
+                </button>
+
+            </div>
+
+            
+        
+        </div>
+
+
+    )
+}
