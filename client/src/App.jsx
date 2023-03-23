e648c09c59c1472f04a8ef011e85c4de04bc8468
Emanuel <marquezema127@gmail.com>
2023-03-23 00:35:37 -0300

    app & index.css optimized

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,20 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
+
     <div className="App">
-      {/* <NavBar/> */}
+
+      {/* <NavBar/> */} {/* Ojo la nav deberia renderizarse aca no dentro del Home/Header, si no no se va a ver en el resto de las vistas */}
 
       <Routes>
+
         <Route path="/" element={<Home />} />
+
       </Routes>
+
     </div>
   );
 }
