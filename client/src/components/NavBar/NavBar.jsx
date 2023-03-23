1d58f89c6533babcd0aab29e64978aff8df7b763
Ezequiel <ezequielresipa45@gmail.com>
2023-03-23 11:32:43 -0300

    Finish Component NavBar

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -2,62 +2,81 @@ import React from 'react'
 import styles from "./NavBar.module.css";
 import logo from "../../images/logo.png"
 import { Link } from 'react-router-dom';
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 export default function NavBar() {
 
-  let arraySpecialists =   [
+  let arraySpecialists = [
     "Clínica Médica",
     "Kinesiología",
-   "Neurología",
-  "Otorrinolaringología",
+    "Neurología",
+    "Otorrinolaringología",
     "Pediatría",
-   "Psiquiatría",
+    "Psiquiatría",
     "Endocrinología",
     "Cardiología",
   ]
 
-const [clickBoolean, setClickBoolean] = useState(false);
+  const [clickBoolean, setClickBoolean] = useState(false);
+
+  let menuRef = useRef();
+
+  useEffect(() => {
+
+    let handler = (e) => {
+
+      if (!menuRef.current.contains(e.target)) {
+        setClickBoolean(false);
+
+      }
+    }
 
 
-const handlerEspeciality = ()=> {
-  
-  clickBoolean ? setClickBoolean(false) : setClickBoolean(true);
-  
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    }
+  })
+
+  const handlerEspeciality = () => {
+
+    clickBoolean ? setClickBoolean(false) : setClickBoolean(true);
+
   };
 
 
   return (
     <>
-    <div className={styles.container__navbar}>
+      <div className={styles.container__navbar}>
 
-      <div className={styles.container__logo}>
-        <Link>
-      <img src={logo} alt="logo" />
-      <p>Vitality Medical Group</p>
-        </Link>
+        <div className={styles.container__logo}>
+          <Link>
+            <img src={logo} alt="logo" />
+            <p>Vitality Medical Group</p>
+          </Link>
+        </div>
+
+        <ul>
+          <Link to='*'>Inicio</Link>
+          <Link to='*'>Institucional</Link>
+          <Link to='*'>Novedades</Link>
+          <Link to='/' style={clickBoolean ? { backgroundColor: "#639cc7", color: "#fff" } : {}} onClick={handlerEspeciality} >Especialidades</Link>
+          <Link to='*'>Planes</Link>
+        </ul>
       </div>
 
-<ul>
-<Link to='*'>Inicio</Link>
-<Link to='*'>Institucional</Link>
-<Link to='*'>Novedades</Link>
-<Link to='/'    style={clickBoolean ? {backgroundColor: "#639cc7", color:"#fff"} : {}}  onClick={handlerEspeciality} >Especialidades</Link>
-<Link to='*'>Planes</Link>
-</ul>
-  </div>
+      {clickBoolean ? (<div className={styles.container__especialitys} ref={menuRef}>
 
-{clickBoolean ? (<div className={styles.container__especialitys}>
-
-<ul>
-{arraySpecialists.map(specialist => (
-  <Link>{specialist}</Link>
-  ))}
-  </ul>
+        <ul>
+          {arraySpecialists.map(specialist => (
+            <Link>{specialist}</Link>
+          ))}
+        </ul>
 
 
-</div>) : ''}
+      </div>) : ''}
 
-  </>
+    </>
   )
 }
