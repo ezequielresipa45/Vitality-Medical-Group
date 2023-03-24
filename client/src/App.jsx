021ecfcd54b996aecd47887e1119db2df1d0dbca
catuhardoy <catalinahardoy@gmail.com>
2023-03-24 16:54:23 -0300

    primer footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home/Home";
 import NavBar from "./components/NavBar/NavBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   
@@ -15,7 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
 
       </Routes>
-
+    <Footer/>
     </div>
 
   );
