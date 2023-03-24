1d14ce22dab7d705ce76071a63e01e84db256f62
catuhardoy <catalinahardoy@gmail.com>
2023-03-24 16:18:41 -0300

    primer footer

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavBar from '../NavBar/NavBar';
 import styles from "./Header.module.css";
+import Footer from '../Footer/Footer';
 
 export default function Header() {
   return (
     <div className={styles.container__header}>
 
-     <NavBar/>   
+     <NavBar/> 
+     <Footer/>  
 
 
 
