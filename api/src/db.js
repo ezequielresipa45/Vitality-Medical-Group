f491082723e33c6418b3205276f25cd224c3921e
Ezequiel <ezequielresipa45@gmail.com>
2023-03-27 21:47:53 -0300

    Deploy Back

--- api/src/db.js
+++ api/src/db.js
@@ -12,15 +12,18 @@ const TicketMedicalModel = require("./models/TicketMedical ");
 const TicketAnalysislModel = require("./models/TicketAnalysis");
 const UserModel = require("./models/User");
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, DB_DEPLOY } =
+  process.env;
 
-const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
-  {
-    logging: false, // set to console.log to see the raw SQL queries
-    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  }
-);
+const sequelize = new Sequelize(DB_DEPLOY, {
+  logging: false, // set to console.log to see the raw SQL queries
+  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
 
 DoctorModel(sequelize);
 PatientModel(sequelize);
