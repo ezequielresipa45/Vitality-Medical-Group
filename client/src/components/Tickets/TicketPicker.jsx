e6626d8c78166665628950c2668b742cebf8f63d
Emanuel <marquezema127@gmail.com>
2023-04-14 19:27:03 -0300

    fix ticket path?

--- client/src/components/Tickets/TicketPicker.jsx
+++ client/src/components/Tickets/TicketPicker.jsx
@@ -28,7 +28,7 @@ const TicketPicker = () => {
     const schedules = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'];
 
     let typeOfTicket;
-    if (selectedTickets.from === '/analisis') typeOfTicket = 'Análisis / Estudio';
+    if (selectedTickets?.from === '/analisis') typeOfTicket = 'Análisis / Estudio';
     else typeOfTicket = 'Consulta Médica';
     
     //const dateFormat = format(date, 'dd/MM/yyyy');
