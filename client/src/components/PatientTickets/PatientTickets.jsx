393336703f755315710558ac9ac6cface9abf0b6
catuhardoy <catalinahardoy@gmail.com>
2023-04-14 11:21:05 -0300

    dashboardPatient

--- client/src/components/PatientTickets/PatientTickets.jsx
+++ client/src/components/PatientTickets/PatientTickets.jsx
@@ -3,44 +3,45 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getPatients, getDoctors } from '../../redux/actions';
 import style from './PatientTickets.module.css';
 
-const PatientTickets = () => {
+
+
+
+const PatientTickets = ({ patient }) => {
+  const doctors = useSelector((state) => state.doctors);
   const user = useSelector((state) => state.user);
   const patients = useSelector((state) => state.patients);
-  const doctors = useSelector((state) => state.doctors);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(getPatients());
-    dispatch(getDoctors());
-  }, []);
+  dispatch(getPatients());
+ dispatch(getDoctors());
+}, []);
 
-  const patientsByLogin = patients.filter((p) => p.userId === user.id);
- 
+
   return (
     <div className={style.div}>
-      <p>Turnos Tomados:</p>
-      {patientsByLogin.length > 0 && (
+      <h2>Tus turnos</h2>
+      {patient.ticketMedicals.length > 0 && (
         <ul className={style.list}>
-          {patientsByLogin[0].ticketMedicals.map((t) => {
-            return (
-              <li key={t.id}>
-                <div className={style['ticket-info']}>
-                  <div>Condición: {t.title}</div>
-                  <div>Doctor/a: {doctors.find((d) => d.id === t.doctorId)?.full_name}</div>
-
-                  <div>Fecha: {t.date}</div>
-                  <div>Hora: {t.hour}</div>
-                </div>
-                <br />
-                <br />
-              </li>
-              
-            );
-          })}
+          {patient.ticketMedicals.map((ticket) => (
+            // <li key={ticket.id}>
+              <div className={style["ticket-info"]}>
+                <h3>Condición: {ticket.title}</h3>
+                <h3>Doctor/a: {doctors.find((d) => d.id === ticket.doctorId)?.full_name}</h3>
+                <h3>Fecha: {ticket.date}</h3>
+                <h3>Hora: {ticket.hour}</h3>
+              </div>
+              // <br />
+              // <br />
+            // </li>
+          ))}
         </ul>
       )}
+      {/* <p>{ticket.date}</p>
+      <p>{ticket.title}</p> */}
     </div>
   );
 };
 
 export default PatientTickets;
+
