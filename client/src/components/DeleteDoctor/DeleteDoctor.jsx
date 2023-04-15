07cc183a318fa9f053cfcc4032ed836958d22308
Ezequiel <ezequielresipa45@gmail.com>
2023-04-15 15:30:49 -0300

    Changes in admin dash

--- client/src/components/DeleteDoctor/DeleteDoctor.jsx
+++ client/src/components/DeleteDoctor/DeleteDoctor.jsx
@@ -1,161 +1,84 @@
 // BORRADO DE MEDICO POR ID
 
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getDoctorsByID, deleteDoctor } from '../../redux/actions';
-import style from './DeleteDoctor.module.css';
-import Swal from 'sweetalert2';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getDoctors, deleteDoctor } from "../../redux/actions";
+import style from "./DeleteDoctor.module.css";
+import Swal from "sweetalert2";
 
 const DeleteDoctor = () => {
-
-
-  const [doctorID, setDoctorID] = useState('');
+  const [doctorID, setDoctorID] = useState("");
   const dispatch = useDispatch();
-  const [doctorInfo, setDoctorInfo] = useState({});
+  // const [doctorInfo, setDoctorInfo] = useState({});
   // Obtener el médico desde el estado global
-  const doctor = useSelector(state => state.doctor);
-  
+  const doctors = useSelector((state) => state.doctors);
+
   useEffect(() => {
-    setDoctorInfo(doctor[0])
-  }, [doctor]);
-  
-  
-  const handleSearch = e => {
-    e.preventDefault();
-    dispatch(getDoctorsByID(doctorID));
-  };
-  
-  const handleDelete = e => {
+    // setDoctorInfo(doctor[0])
+    dispatch(getDoctors());
+  }, []);
+
+  // const handleSearch = e => {
+  //   e.preventDefault();
+  //   dispatch(getDoctorsByID(doctorID));
+  // };
+
+  const handleDelete = (e, data) => {
     e.preventDefault();
     Swal.fire({
-      title: 'Está seguro?',
+      title: "Está seguro?",
       text: "No podrá revertir los cambios!",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si, eliminar!'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar!",
     }).then((result) => {
       if (result.isConfirmed) {
-        dispatch(deleteDoctor(doctorID));
-        setDoctorID('');
-        Swal.fire(
-          'Eliminado!',
-          'El médico ah sido eliminado.',
-          'Exitosamente'
-        )
+        dispatch(deleteDoctor(data));
+        Swal.fire("Eliminado!", "El médico ah sido eliminado.", "Exitosamente")
+        .then(()=>{
+          dispatch(getDoctors());
+
+        })
       }
-    })
-    
-  }
-  
+    });
+  };
+
   return (
     <div className={style.container__deleteMedic}>
+      <div className={style.tablesName}>
+        <p>Perfíl</p>
+        <p>Nombre</p>
+        <p>Dirección</p>
+        <p>Género</p>
+        <p>Edad</p>
+        <p>Matrícula</p>
+        <p></p>
+      </div>
 
-
-
-      <form onSubmit={handleSearch}>
-        <input
-          type="text"
-          id="doctorID"
-          value={doctorID}
-      placeholder='Ingrese id'
-          onChange={e => setDoctorID(e.target.value)}
-          />
-        <button type="submit">
-        <i class="fas fa-search"></i>
-        Buscar Médico 
-        </button>
-      </form>
-
-<div className={style.tablesName}>
-
-  <p>Perfíl</p>
-  <p>Nombre</p>
-  <p>Dirección</p>
-  <p>Género</p>
-  <p>Edad</p>
-  <p>Matrícula</p>
-  <p></p>
-</div>
-
-      {doctor.hasOwnProperty("full_name") && (
-        <div className={style.medicInfo}>
-          <div className={style.container__image}>
-        <img src={doctor.image} alt={doctor.full_name} />
-
+      {doctors &&
+        doctors.map((doctor) => (
+          <div className={style.medicInfo}>
+            <div className={style.container__image}>
+              <img src={doctor.image} alt={doctor.full_name} />
+            </div>
+            <p>{doctor.full_name}</p>
+            <p>{doctor.address}</p>
+            <p>{doctor.gender}</p>
+            <p>{doctor.age}</p>
+            <p>{doctor.code}</p>
+            <button
+              className={style.button}
+              onClick={(e) => handleDelete(e, doctor.id)}
+            >
+              <i class="fas fa-trash-alt"> </i> Eliminar{" "}
+            </button>
+            {/* Puedes mostrar otros detalles del médico */}
           </div>
-          <p>{doctor.full_name}</p>
-          <p>{doctor.address}</p>
-          <p>{doctor.gender}</p>
-          <p>{doctor.age}</p>
-          <p>{doctor.code}</p>
-          <button className={style.button} onClick={handleDelete}><i class="fas fa-trash-alt"> </i> Eliminar </button>
-          {/* Puedes mostrar otros detalles del médico */}
-        </div>
-      )}
-
-
-{doctor === "No existe Médico con ese Id" && ( <p>No existe Médico con ese Id</p>)}
-
+        ))}
     </div>
   );
 };
 
 export default DeleteDoctor;
-
-
-
-
-
-
-// import React, { useEffect, useState } from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { getDoctorsByID } from '../../redux/actions';
-
-// const AdminDelete = () => {
-//   const [doctorID, setDoctorID] = useState('');
-//   const dispatch = useDispatch();
-//   const [doctorInfo, setDoctorInfo] = useState({});
-//   // Obtener el médico desde el estado global
-//   const doctor = useSelector(state => state.doctor);
-//   useEffect(()=>{
-//   setDoctorInfo(doctor[0])
-// },[doctor]);
-
-// console.log(doctor);
-//   const handleSearch = e => {
-//     e.preventDefault();
-//     dispatch(getDoctorsByID(doctorID));
-//   };
-
-  
-
-//   return (
-//     <div>
-//       <h1>Buscar médico por ID</h1>
-//       <form onSubmit={handleSearch}>
-//         <label htmlFor="doctorID">ID del médico:</label>
-//         <input
-//           type="text"
-//           id="doctorID"
-//           value={doctorID}
-//           onChange={e => setDoctorID(e.target.value)}
-//         />
-//         <button type="submit">Buscar</button>
-//       </form>
-
-//       {doctor && (
-//         <div>
-//           <h2>{doctor.full_name}</h2>
-//           <p>Especialidad: {doctor.phone}</p>
-//           <p>Horario: {doctor.address}</p>
-//           {/* Puedes mostrar otros detalles del médico */}
-//         </div>
-//       )}
-      
-//     </div>
-//   );
-// };
-
-// export default AdminDelete;
