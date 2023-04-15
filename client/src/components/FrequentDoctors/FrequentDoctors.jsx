bff037a3763f173375cf2b495c644c25f10c69bf
catuhardoy <catalinahardoy@gmail.com>
2023-04-15 16:16:42 -0300

    dashboard/frequent

--- client/src/components/FrequentDoctors/FrequentDoctors.jsx
+++ client/src/components/FrequentDoctors/FrequentDoctors.jsx
@@ -1,65 +1,43 @@
+
 import React from "react";
 import {useState} from "react"
 import { Typography } from "@mui/material";
 import  { useEffect } from 'react';
-import { getDoctors } from "../../redux/actions";
+import { getDoctors, getPatients } from "../../redux/actions";
 import { useDispatch } from "react-redux";
 import { useSelector } from "react-redux";
 
 
-const FrequentDoctors = ({frequent}) => {
- 
-  const dispatch = useDispatch()
-  const [frequentDoctors, setFrequentDoctors] = useState([])
+const FrequentDoctors = ({patient}) => {
+  
   const doctors = useSelector((state)=>state.doctors)
- 
+  // const patients =useSelector((state)=>state.patients)
 
+  //traer paciente x id con useLocation
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
    dispatch(getDoctors());
+   dispatch(getPatients())
   }, [dispatch]);
 
 
-  const countDoctors = (tickets) => {
-    const doctorCounts = {};
-    tickets.forEach((ticket) => {
-      // console.log("countDoc:"countDoctors)
-      const doctorId = ticket.doctorId;
-      console.log("docID:", doctorId)
-      if (!doctorCounts[doctorId]) {
-        doctorCounts[doctorId] = 1;
-      } else {
-        doctorCounts[doctorId]++;
-      }
-    });
-    return doctorCounts;
+  const doctorIds = patient.ticketMedicals.map((ticket) => ticket.doctorId);  // busco en los ticketMedical los id de los doctores
+  const frequentDoctors = doctors.filter((doctor) => doctorIds.includes(doctor.id));  // filtro los doctores que tengan mismo id que doctorIds 
 
-    
-  };
-  useEffect(()=> {
-    if(frequent && frequent[0]){
-      console.log("frequent:", frequent)
-      const frequentDoctorId = frequent[0]
-      const doctor = doctors.find((doctor) => doctor.id === frequentDoctorId);
-      setFrequentDoctors([doctor]);
-      console.log(doctor)
-
-    }
-  },[]) 
- 
+console.log(frequentDoctors)
   return (
     <>
-      <Typography>Médicos frecuentes:</Typography>
-
-      {frequentDoctors.length > 0 ? (
+       {frequentDoctors.length > 0 ? (
+    <div>
+      <h2>Médicos más frecuentes</h2>
       <ul>
         {frequentDoctors.map((doctor) => (
-          <li key={doctor.id}>
-
-          <h3> {doctors.find((d) => d.id === ticket.frequent)?.full_name}</h3>
-          </li>
+          <li key={doctor.id}>{doctor.full_name}</li>
         ))}
       </ul>
+    </div>
       ):(
         <Typography>"No hay suficiente informacion para mostrar medicos frecuentes"</Typography>
       )}
