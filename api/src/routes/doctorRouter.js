5ba8621e36567314401652f512bcb9c5ff17f720
moyobear <rodriguezjmm@hotmail.com>
2023-04-20 20:43:55 -0400

    Eliminación de protección de rutas de Doctor

--- api/src/routes/doctorRouter.js
+++ api/src/routes/doctorRouter.js
@@ -3,7 +3,7 @@ const {
   validatorCreateDoctor,
   validatorUpdateDoctor,
 } = require("../middlewares/validators.js");
-const jwtCheck = require("../middlewares/auth.js");
+
 const {
   getNamesHandler,
   getDoctorsDeletedHandler,
@@ -39,8 +39,8 @@ doctorRouter.put("/recoverDoctor", recoverDoctorHandler);
 
 doctorRouter.put("/medicalGuard", updateMedicalGuardHandler);
 
-doctorRouter.delete("/delSchedule", jwtCheck, deleteScheduleHandler);
+doctorRouter.delete("/delSchedule", deleteScheduleHandler);
 
-doctorRouter.delete("/:id/delete", jwtCheck,  deleteDoctorHandler);
+doctorRouter.delete("/:id/delete", deleteDoctorHandler);
 
 module.exports = doctorRouter;
